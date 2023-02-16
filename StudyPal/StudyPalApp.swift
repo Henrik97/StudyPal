ecc5c89c423e9826fb1b6697ef82474d6965fb25
Chenxi <chen0046@hotmail.com>
2023-02-16 14:43:35 +0100

    Initial Commit

---
+++ StudyPal/StudyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPalApp.swift
+//  StudyPal
+//
+//  Created by Chenxi Cai on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
