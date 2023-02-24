c459beffbdb673839c57bf2a45d6220e20aeb7e0
Chenxi <chen0046@hotmail.com>
2023-02-24 10:20:34 +0100

    LoginView and LoginDetail

---
+++ StudyPal/LogInView.swift
@@ -0,0 +1,29 @@
+//
+//  LogInView.swift
+//  StudyPal
+//
+//  Created by Chenxi Cai on 23/02/2023.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+
+    
+    var body: some View {
+        
+        VStack{
+            LoginDetail()
+           
+
+
+            
+        }.padding()
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
